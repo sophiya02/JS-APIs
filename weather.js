54b18f4e227253aaee8be5352ebada7d5b6ba464
sophiya02 <sophiya.singh2002@gmail.com>
2022-02-07 22:15:45 +0530

    added files

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 let w_api_key = config.WEATHER_API_KEY;
 let submit = document.getElementById("submit");
 let city = document.getElementById("city");
+let fail = document.getElementById("fail");
 let cityValue = "london";
 async function getWeatherData() {
 
@@ -8,33 +9,51 @@ async function getWeatherData() {
         console.log(cityValue);
         let wres = await fetch(`http://api.weatherapi.com/v1/current.json?key=9ec8d3e2584f417397685005220702&q=${cityValue}&aqi=no`);
         let wdata = await wres.json();
-        console.log(wdata);
+        // console.log(wdata);
         return wdata;
-    }
+        }   
     catch (error) {
-
-        return -1;
+        // console.log(error);
+        fail.innerText = `${error} `;
+        showFail();
     }
 }
+
 async function disData() {
     let wdata = await getWeatherData();
-    console.log(wdata);
-    if (wdata != -1) {
-        document.getElementById("ele1").innerText = `Temp in C= ${wdata.current.temp_c} deg celcius `;
-        document.getElementById("ele2").innerText = `Temp in F= ${wdata.current.temp_f} fahrenhiet `;
-        document.getElementById("ele3").innerText = `Cloud = ${wdata.current.cloud}  `;
-        document.getElementById("ele4").innerText = `Wind direction= ${wdata.current.wind_dir}  `;
-        document.getElementById("ele5").innerText = `Humidity= ${wdata.current.humidity} `;
-        document.getElementById("ele6").innerText = `wind speed= ${wdata.current.wind_kph} kph `;
-        document.getElementById("ele7").innerText = `Location= ${wdata.location.country}  `;
-        document.getElementById("ele8").innerText = `Localtime= ${wdata.location.localtime}  `;
-        document.getElementById("ele9").innerText = `Region= ${wdata.location.region}  `;
-        document.getElementById("ele10").innerText = `Day: ${wdata.current.condition.text} `;
+    // console.log(wdata);
+    if(!wdata.error) {
+        
+        document.getElementById("ele1").innerText = `${wdata.current.condition.text} `;
+        document.getElementById("ele2").src= `https:${wdata.current.condition.icon}`;
+        // console.log(typeof(wdata.current.condition.icon));
+        document.getElementById("ele3").innerText = `Humidity: ${wdata.current.humidity} `;
+        document.getElementById("ele4").innerText = `${wdata.current.temp_c} deg C`;
+        document.getElementById("ele5").innerText = `${wdata.current.temp_f} F `;
+        document.getElementById("ele6").innerText = `Wind: ${wdata.current.wind_dir} direction `;
+        document.getElementById("ele7").innerText = `at: ${wdata.current.wind_kph} kph `;
+        document.getElementById("ele8").innerText = `${wdata.location.region}  `;
+        document.getElementById("ele9").innerText = `${wdata.location.country}  `;
+        document.getElementById("ele10").innerText= `${wdata.location.localtime}  `;
+        showSuccess();
     }
     else
-        document.getElementById("ele1").innerText = `error occured `;
+     {  
+          fail.innerText = `${wdata.error.message} `;
+          showFail();
+        }
 
 }
+function showFail(){
+    displayData.style.display="none";
+    fail.style.display="block";
+    
+}
+function showSuccess(){
+    fail.style.display="none";
+    displayData.style.display="grid";
+}
+// console.log(document.getElementById("ele2").src);
 submit.addEventListener("click", () => {
     cityValue = document.getElementById("city").value;
     // console.log(cityValue);
